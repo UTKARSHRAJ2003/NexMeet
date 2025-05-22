09e88ef66b163c639ef101eacdc495d2c269db29
Utkarsh Raj <utkarshrajbkp@gmail.com>
2025-05-22 15:08:57 +0530

    update

--- backned/src/app.js
+++ backned/src/app.js
@@ -1,39 +1,39 @@
 import express from "express";
 import { createServer } from "node:http";
-
 import { Server } from "socket.io";
-
 import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
-
 import cors from "cors";
-import userRoutes from "./routes/users.routes.js";
+import usersRoutes from './routes/users.routes.js';
+
 
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
+app.set("port", process.env.PORT || 8001);
 
-app.set("port", (process.env.PORT || 8000))
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/users", usersRoutes);
+
 
 const start = async () => {
-    app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
+  try {
+    const connectionDb = await mongoose.connect(
+      "mongodb+srv://techutkarsh_raj:Techutkarsh@cluster0.iioyo5r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
 
-    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 8000")
+      console.log(`LISTENING ON PORT ${app.get("port")}`);
     });
-
-
-
-}
-
-
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
 
 start();
